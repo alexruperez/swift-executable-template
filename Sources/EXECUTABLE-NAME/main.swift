f5daae594c686844a81036f765656fa69899f00c
Alex Rupérez <me@alexruperez.com>
2020-04-16 18:49:50 +0200

    Sample struct is now public.

--- Sources/EXECUTABLE-NAME/main.swift
+++ Sources/EXECUTABLE-NAME/main.swift
@@ -5,7 +5,11 @@ public struct EXECUTABLE-NAME: ParsableCommand {
     @Flag(help: "Show SwiftExecutableTemplate version.")
     var version: Bool
 
-    func run() throws {
+    /// Create EXECUTABLE-NAME.
+    public init() {}
+
+    /// Run EXECUTABLE-NAME.
+    public func run() throws {
         guard !version else {
             print("EXECUTABLE-NAME v0.1.0")
             return
